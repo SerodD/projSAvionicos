8c8274b99271a6ac4b3e536b09c8bcd61e912b64
SerodD <diogo.dores94@gmail.com>
2018-10-29 18:29:06 +0000

    Revert "funçao para o V_m"
    This reverts commit 8a05ce3df6a89189a73d9599d0d601dfeaa9d234.

--- fms2.c
+++ fms2.c
@@ -16,7 +16,11 @@
 #define ALPHA 0.001
 #define KMH_TO_MS 1000/3600
 #define TIME_ACEL 180
+<<<<<<< HEAD
 #define T 20*60
+=======
+#define 
+>>>>>>> parent of 8a05ce3... funçao para o V_m
 
 void process_points(char point_1[NB_DATA][ELE_SIZE], char point_2[NB_DATA][ELE_SIZE], double **info) { //função para calcular distância entre 2 pontos consecutivos
 
@@ -98,6 +102,7 @@ double calculate_theta_path(double V_TAS, double height_dev) {
 	return theta_path;
 }
 
+<<<<<<< HEAD
 double calculate_V_m(double V_TAS, double delta_time) {
 	double V_m = V_TAS*(1 + 0.01*sin(2*PI*delta_time/T));
 	return V_m;
@@ -113,6 +118,13 @@ int main() {
 	FILE *file;
 	double route_distance = 0, time_between_points = 0, total_route_distance = 0, height_dev = 0, height = 0, true_heading = 0, theta_path = 0, time_div = 0, delta_time = 0, true_air_speed = 0, v_ref = 0;
 	double *info, *velocity_N_E, *info_m;
+=======
+int main() {
+
+	FILE *file;
+	double route_distance = 0, time_between_points = 0, total_route_distance = 0, height_dev = 0, height = 0, true_heading = 0, theta_path = 0, time_div = 0;
+	double *info, *velocity_N_E;
+>>>>>>> parent of 8a05ce3... funçao para o V_m
 	char *ch, line[DIM], point_1[NB_DATA][ELE_SIZE], point_2[NB_DATA][ELE_SIZE];
 	int i = 0, j = 0;
 	time_t seconds_prev, seconds_init, seconds_act;
@@ -178,11 +190,13 @@ int main() {
 			calculate_velocity_N_E(&velocity_N_E, true_air_speed, theta_path, true_heading);
 
 			
+
+			// processar caminho (isto agora vai estar meio preso aqui, porque o tempo não está muito acelerado)
+
 			while(dist_btw_2points(info) > 10000) {
 				seconds_act = time(NULL);
 				if (((double)seconds_act - (double)seconds_prev) >= 1) {
 					time_div = ((double)seconds_act - (double)seconds_prev) * TIME_ACEL;
-					delta_time = (double)seconds_act - (double)seconds_init;
 					seconds_prev = seconds_act;
 					true_air_speed = controller_actuator(calculate_V_m(true_air_speed, delta_time), v_ref); //controlador e atuador juntos
 					height = height + (height_dev * time_div);
